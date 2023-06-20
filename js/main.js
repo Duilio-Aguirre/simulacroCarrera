c6b6e1f4fc7893fe6b54279fd5b7f7d19e78e06d
Duilio-Aguirre <duilionicolas@gmail.com>
2023-06-20 19:23:01 -0300

    new commit

--- js/main.js
+++ js/main.js
@@ -1,16 +1,16 @@
+let millasCamaro = 300;
 
-let velocidadMaximaCamaro = 100;
+let millasMustang = 300;
 
-let velocidadMaximaMustang = 100;
+let millasChallenger = 300;
 
-let velocidadMaximaChallenger = 100;
+while (millasCamaro>0 && millasMustang>0 && millasChallenger>0) {
 
 let velocidadCamaro = Math.ceil(Math.random ()*100);
 
 let velocidadMustang = Math.ceil(Math.random ()*100);
 
 let velocidadChallenger = Math.ceil(Math.random ()*100);
-
 
 console.log ("El Camaro avanza a una velocidad de " + (velocidadCamaro) + "millas por hora");
 
@@ -20,8 +20,25 @@ console.log ("El Challenger avanza a una velocidad de " + (velocidadChallenger) 
 
 console.log ("-------------------------------------------------------------------------")
 
+millasCamaro = millasCamaro - velocidadCamaro;
+millasMustang = millasMustang - velocidadMustang;
+millasChallenger = millasChallenger - velocidadChallenger
+
 if(velocidadCamaro>velocidadMustang && velocidadCamaro>velocidadChallenger) {
-    console.log ("El Camaro toma la delantera y es el ganador")
+    millasCamaro -= velocidadCamaro
+    console.log ("El Camaro toma la delantera y le quedan ", (millasCamaro) , "por recorrer");
 } else if (velocidadMustang>velocidadCamaro && velocidadMustang>velocidadChallenger) {
-    console.log ("El Mustang toma la delantera y es el ganador")
-}else {console.log ("El Challenger toma la delantera y es el ganador")} 
+    millasMustang -= velocidadMustang
+    console.log ("El Mustang toma la delantera y le quedan ", (millasMustang) , "por recorrer");
+}else {
+    millasChallenger -= velocidadChallenger
+    console.log ("El Challenger toma la delantera y le quedan ", (millasChallenger), "por recorrer");
+}
+
+if(millasCamaro<=0){
+console.log ("El Camaro es el ganador"
+} else if (millasMustang<=0) {
+    console.log ("El Mustang es el ganador")
+} else { 
+    console.log ("El Challenger es el ganador")}
+}
